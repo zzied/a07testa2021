f1724989c414e8964870441b6a222fc88b82ba74
z.zaier <z.zaier@5580-LAP-13.interne.college-gerald-godin.qc.ca>
2021-11-08 16:38:48 -0500

    Ajout de la classe Zied

---
+++ src/javaapplication29/Zied.java
@@ -0,0 +1,39 @@
+package javaapplication29;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author z.zaier
+ */
+public class Zied {
+    private int id;
+    private String name;
+
+    public Zied(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+   
+    
+}
